762ef43b1f0c144cfa98154c1bf213dfcd7e31b8
Alexander Schenkel <alex@alexi.ch>
2015-02-15 17:57:56 +0100

    Build system update

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,48 +1,53 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
-    jasmine: {
-      customTemplate: {
-        src: [
-          'src/Prototype.Model.js',
-          'src/Prototype.Model.sync.js'
-        ],
-        options: {
-          specs: ['specs/**/*Spec.js'],
-          helpers: 'specs/**/*Helper.js',
-          vendor: [
-            "specs/prototype.js",
-          ]
-        }
-      }
-    },
-
-    uglify: {
-      dist: {
-        options: {
-          preserveComments: false
+    grunt.initConfig({
+        jasmine: {
+            customTemplate: {
+                src: [
+                    'src/Prototype.Model.js',
+                    'src/Prototype.Model.sync.js'
+                ],
+                options: {
+                    specs: ['specs/**/*Spec.js'],
+                    helpers: 'specs/**/*Helper.js',
+                    vendor: [
+                        "specs/prototype.js",
+                    ]
+                }
+            }
         },
-        files: {
-          'dist/prototype-model.min.js': ['src/Prototype.Model.js', 'src/Prototype.Model.sync.js']
-        }
-      },
-      distDebug: {
-        options: {
-          mangle: false,
-          compress: false,
-          sourceMap: true,
-          preserveComments: 'all',
-          beautify: true
+
+        uglify: {
+            dist: {
+                options: {
+                    preserveComments: false
+                },
+                files: {
+                    'dist/prototype-model.min.js': ['src/Prototype.Model.js', 'src/Prototype.Model.sync.js']
+                }
+            },
+            distDebug: {
+                options: {
+                    mangle: false,
+                    compress: false,
+                    sourceMap: true,
+                    preserveComments: 'all',
+                    beautify: true
+                },
+                files: {
+                    'dist/prototype-model.debug.js': ['src/Prototype.Model.js', 'src/Prototype.Model.sync.js']
+                }
+            }
         },
-        files: {
-          'dist/prototype-model.debug.js': ['src/Prototype.Model.js', 'src/Prototype.Model.sync.js']
+
+        jshint: {
+            all: ['Gruntfile.js', 'src/**/*.js', 'specs/**/*.js','!specs/prototype.js']
         }
-      }
-    }
-  });
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-jasmine');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jasmine');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
 
-  grunt.registerTask('test', 'Run test suite using jasmine', ['jasmine']);
-  grunt.registerTask('default', ['test']);
+    grunt.registerTask('test', 'Run test suite using jasmine', ['jasmine']);
+    grunt.registerTask('default', ['test']);
 };
