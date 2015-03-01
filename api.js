b59706d44016f66def39d87e7fae4d5267eb6855
Alexander Schenkel <aschenkel@kadenpartner.ch>
2015-03-01 22:01:57 +0100

    doc update

--- api.js
+++ api.js
@@ -1,6 +1,8 @@
 YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
+        "Protobone.Base",
+        "Protobone.Collection",
         "Protobone.Model"
     ],
     "modules": [],
